48f929b70803a107f9702e70df1431e8d4740175
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-05-17 19:18:35 -0300

    create a class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-        Hello word!
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome : 'Flavio'
+  }
+
+  render(){
+    return(
+      <div>
+          <label>Hello {this.state.nome} </label>
+      </div>
+    )
+  }
 }
 
 export default App;
