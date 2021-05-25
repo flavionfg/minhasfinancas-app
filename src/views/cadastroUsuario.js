4a98ae663cc407d13df118d48fdb96301f6f5225
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-05-24 22:04:05 -0300

    create NavbarItems and add on Navbar
    precisamos passar os valores para o navBar (no exemplo acima seria o home.html e Home), vamos parametrizar eles usando props passando como propriedade
    
    vamos passar o parametro via interpolação.
    
    ficando assim : <a className="nav-link" href={props.href}>{props.label}</a>
    
    importar o navBarItems no navBar, quando o arquivo esta na mesma pasta usamos apenas um . antes da barra : import NavbarItem from './navbarItem'
    
    na hora de adicionar os navBarItems no Navbar como estamos usando link <a> temos que usar o # no href="#/" pois é do Hashrouter , usar antes de colocar a rota

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -18,7 +18,6 @@ class CadastroUsuario extends React.Component{
 
     render(){
         return(
-            <div className="container">
                 <Card title="Cadastro de Usuario">
                    <div className="row">
                        <div className="col-lg-l2">
@@ -57,7 +56,6 @@ class CadastroUsuario extends React.Component{
                        </div>
                    </div>
                 </Card>
-            </div>
         )
     }
 }
--- src/views/login.js
+++ src/views/login.js
@@ -18,7 +18,6 @@ class Login extends React.Component{
     render(){
         return(
 
-        <div className="container">
             <div className="row">
                 <div className="col-md-6" style={ {position : 'relative', left: '300px'} }>
                     <div className="bs-docs-section">
@@ -53,7 +52,6 @@ class Login extends React.Component{
                     </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
