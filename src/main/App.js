0245538daeb4c43ae1ad32d1d45436db2ba25b73
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-06-03 12:01:57 -0300

    Adding and testing the primereact library in the project
    npm install primereact --save
    npm install primeicons --save

--- src/main/App.js
+++ src/main/App.js
@@ -3,6 +3,12 @@ import React from 'react';
 import Rotas from './rotas'
 import Navbar from '../components/navbar'
 
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css'
+
+import {Button} from 'primereact/button';
+
 import 'toastr/build/toastr.min.js'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
@@ -10,11 +16,13 @@ import '../custom.css'
 import 'toastr/build/toastr.css'
 
 
+
 class App extends React.Component {
   render(){
     return(
       <>
         <Navbar />
+        <Button label="Click" icon="pi pi-check" iconPos="right" />
         <div className="container">
             <Rotas />  
         </div>
